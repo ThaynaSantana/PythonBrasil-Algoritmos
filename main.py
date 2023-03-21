dec8f1cdaeea550d6d605e2e3f6e50b5e8ca4c87
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2023-03-21 00:37:52 +0000

    Add import dentro da pasta dos exercicios WORKING!

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# saber importar dentro do diretorio um arquivo
+
+import sys
+
+sys.path.insert(1, './estruturasequencial')
+
 import x002
+# este arquivo main e somente necessario para utilizar com o replit.com! se nao IGNORE
